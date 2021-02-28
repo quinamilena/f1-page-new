4dbd6ab96bd67a927d7dd59a433c6c114ddb4ad3
Milena Quina <milena.mqx@live.com>
2021-02-28 14:36:25 -0300

    Alterações de hoje

--- src/app/pages/circuits/circuits.component.ts
+++ src/app/pages/circuits/circuits.component.ts
@@ -13,6 +13,21 @@ export class CircuitsComponent implements OnInit {
     return this.NavScroll.navScroll();
   }
 
+  getYearsCircuits(): Array<number> {
+    let endYears: number = 1950;
+    let today: Date = new Date();
+    let getYear: number = today.getFullYear();
+    let allYears: Array<number> = Array();
+
+    for (let index = getYear; index >= endYears; index--) {
+      allYears.push(index);
+    }
+
+    return allYears;
+  }
+
+  years = this.getYearsCircuits();
+
   ngOnInit(): void {
     window.scrollTo(0, 0);
     this.navScrollFunction();
