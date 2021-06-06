1b521b2b41aed49074650a6c28877f54faa08248
Milena Quina <milena.mqx@live.com>
2021-06-06 19:43:32 -0300

    Correções no input de filtro

--- src/app/services/select-filter.service.ts
+++ src/app/services/select-filter.service.ts
@@ -7,17 +7,17 @@ export class SelectFilterService {
   constructor() {}
 
   funcSelect(): void {
-    const allOptions: NodeListOf<Element> = document.querySelectorAll(
-      '.default_option'
-    );
-    const dropdownAll: NodeListOf<Element> = document.querySelectorAll(
-      '.dropdown ul'
-    );
-    const allLi: NodeListOf<Element> = document.querySelectorAll(
-      '.dropdown ul li'
-    );
+    const allOptions: NodeListOf<Element> =
+      document.querySelectorAll('.default_option');
+    const dropdownAll: NodeListOf<Element> =
+      document.querySelectorAll('.dropdown ul');
+    const dropdownDiv = document.querySelectorAll('.dropdown');
+    const allLi: NodeListOf<Element> =
+      document.querySelectorAll('.dropdown ul li');
 
     allOptions[0].addEventListener('click', (): void => {
+      dropdownDiv[0].classList.toggle('downup');
+
       dropdownAll.forEach((item: any) => {
         item.classList.toggle('active');
       });
@@ -25,6 +25,8 @@ export class SelectFilterService {
 
     allLi.forEach((item) => {
       item.addEventListener('click', (): void => {
+        dropdownDiv[0].classList.toggle('downup');
+
         const text: any = item.innerHTML;
         const defaultClass: any = document.querySelector('.default_option');
         defaultClass.innerHTML = text;
